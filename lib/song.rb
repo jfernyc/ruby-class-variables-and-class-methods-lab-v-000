43dbd1139ddf917d715841d1e1f92eab168c6911
J Fern <jfern20@gmail.com>
2019-03-13 01:31:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,15 +33,7 @@ class Song
   
   
   def self.genre_count 
-    genre_count = {}
-    @@genres.each do |genre|
-    if genre_count [genre]
-      genre_count[genre] +=1 
-    else 
-      genre_count[genre] = 1 
-    end 
-  end 
-    genre_count
+  @@genres.inject(Hash.new(0))
   end 
   
   
